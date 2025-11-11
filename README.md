028b3b908e0db885088248d3afe8e2668f740762
USAMI Kenta <tadsan@zonu.me>
2025-11-11 19:56:46 +0900

    Init repository

---
+++ README.md
@@ -0,0 +1,15 @@
+## Debug repository for EasyCS
+
+## Subject
+
+```
+EasyCodingStandard 13.0.0
++ PHP_CodeSniffer 4.0.0
++ PHP-CS-Fixer 3.89.1
+```
+
+## How to reproduce the problem
+
+```
+rm -rf .ecs_cache && ./vendor/bin/ecs check --debug
+```
---
+++ ecs.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([__DIR__, __DIR__ . '/src'])
+    ->withPreparedSets(cleanCode: true)
+    ->withCache(directory: __DIR__ . '/.ecs_cache')
+    ->withSkip([__DIR__ . '/.ecs_cache']);
